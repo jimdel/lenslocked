0676fee489af0e06c9b6da9e829a82a976d3b8e2
James DeLay <jdelay.jr@gmail.com>
2024-08-22 12:51:27 -0400

    installing chi router

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const PORT = ":42069"
@@ -14,78 +17,38 @@ func defaultHandlerFunc(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "<h1>Happy, healthy, wealthy Jimbroski!</h1>")
 }
 
-func contactHandlerFunc(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact</h1><p>Get in touch at <a href=\"mailto:jimdel@gmail.com\">jimdel</a></p>")
-}
-
-func pathHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.RawPath)
-}
-
 func faqHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	contents, err := os.ReadFile("faq.txt")
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Something terrible occured!", http.StatusInternalServerError)
+	} else {
+		rawFaq := string(contents)
+		var faqHtml string
+		for _, line := range strings.Split(rawFaq, "\n") {
+			faqHtml += "<p>" + line + "</p>"
+		}
+		w.Header().Set("Content-Type", "text/html;charset=utf-8")
+		fmt.Fprint(w, faqHtml)
 	}
-	rawFaq := string(contents)
-	var faqHtml string
-	for _, line := range strings.Split(rawFaq, "\n") {
-		faqHtml += "<p>" + line + "</p>"
-	}
 
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	fmt.Fprint(w, faqHtml)
-}
-
-// METHOD 1 - create a Router type & implement ServeHTTP method
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/path":
-// 		pathHandlerFunc(w, r)
-// 	case "/contact":
-// 		contactHandlerFunc(w, r)
-// 	case "/":
-// 		defaultHandlerFunc(w, r)
-// 	default:
-// 		http.Error(w, "404 NOT FOUND", http.StatusNotFound)
-// 	}
-// }
-
-func routerFunc(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/path":
-		pathHandlerFunc(w, r)
-	case "/contact":
-		contactHandlerFunc(w, r)
-	case "/faq":
-		faqHandlerFunc(w, r)
-	case "/":
-		defaultHandlerFunc(w, r)
-	default:
-		http.Error(w, "404 NOT FOUND", http.StatusNotFound)
-	}
 }
 
 func main() {
 
 	fmt.Printf("Server listening on port %v\n", PORT)
-	// Routes
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	// METHOD 1 - create a Router type & implement ServeHTTP method
-	// var router Router
-	// err := http.ListenAndServe(PORT, router)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "404 - Not Found!", http.StatusNotFound)
+	})
 
-	// METHOD 2 - create an
-	// var router http.HandlerFunc = routerFunc
+	r.Get("/faq", faqHandlerFunc)
+	r.Get("/", defaultHandlerFunc)
 
-	// METHOD 3
-	err := http.ListenAndServe(PORT, http.HandlerFunc(routerFunc))
-
+	err := http.ListenAndServe(PORT, r)
 	if err != nil {
 		fmt.Printf("<< SERVER ERROR >>")
 		panic(err)
