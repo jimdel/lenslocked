6eff3e84f421416eecefc932f528a355dd854db1
jimdel <jdelay.jr@gmail.com>
2024-12-17 14:46:28 -0500

    feat: adding a query param to create user

--- controllers/users.go
+++ controllers/users.go
@@ -12,7 +12,11 @@ type User struct {
 }
 
 func (u User) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
 }
 
 func (u User) Create(w http.ResponseWriter, r *http.Request) {
