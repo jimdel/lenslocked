bd4a642d6557705e412abff320a6dfb6fcd8d97e
jimdel <jdelay.jr@gmail.com>
2025-01-01 17:38:37 -0500

    rm old sql files

--- migrations/00002_sessions.sql
+++ migrations/00002_sessions.sql
@@ -15,3 +15,22 @@ CREATE TABLE sessions (
 -- +goose StatementBegin
 DROP TABLE sessions;
 -- +goose StatementEnd
+
+
+-- CREATE TABLE sessions (
+    -- id SERIAL PRIMARY KEY,
+    -- Inline method to declare foreign key 
+        -- user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
+    -- Alternative method to declare foreign key:
+        -- FOREIGN KEY (user_id) REFERENCES users (id)
+-- );
+
+-- Add foreign key to existing table
+-- ALTER TABLE sessions
+-- ADD CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id);
+
+-- To delete all entries when deleting a parent entry:
+-- ON DELETE CASCADE
+
+-- Create an index
+-- CREATE INDEX sessions_token_hash_idx ON sessions (token_hash, user_id, id);
